fe0a46d11dea79123ad18eda3bab4e71b2153552
Mark McDermott <mark@markmcdermott.io>
2026-02-18 17:40:31 -0600

    remove mp4s

--- playwright.demo.config.ts
+++ playwright.demo.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests/demos/scripts',
   testMatch: '*.demo.ts',
-  timeout: 180_000,
+  timeout: 900_000,
   fullyParallel: false,
   workers: 1,
   reporter: [['list']],
